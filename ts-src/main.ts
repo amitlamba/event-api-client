7a468d22bb6d360cb598b8412eda0092dcfd447a
Amit Lamba <amitlamba4198@gmail.com>
2018-06-07 15:49:59 +0530

    Enhancements

--- ts-src/main.ts
+++ ts-src/main.ts
@@ -3,13 +3,6 @@ import HttpClient from "./HttpClient";
 import {baseuri, eventuri, initializeuri, profileuri} from "./Constants";
 
 
-// export class Sender {
-//     storage: any[] = [];
-//     push(obj: any) {
-//         this.storage.push(obj);
-//         repeat();
-//     }
-// }
 
 declare var _und: any;
 
@@ -19,6 +12,12 @@ _und = _und || {event: [], profile: [], account: [], onUserLogin: [], notificati
 var httpClient = new HttpClient(_und.account[0].id);
 var identity: Identity = {} as Identity;
 var user: EventUser = {} as EventUser;
+
+function getURLParameter(name: any) {
+    // return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+    if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+        return decodeURIComponent(name[1]);
+}
 
 function getIdentity() {
     httpClient.postData(baseuri + initializeuri).then(
@@ -30,6 +29,29 @@ function getIdentity() {
     );
 }
 
+function removeIdentity() {
+    localStorage.removeItem("id");
+}
+
+function setUtmParams() {
+    if (getURLParameter('utm_source') || getURLParameter('utm_medium') || getURLParameter('utm_campaign')
+        || getURLParameter('utm_term') || getURLParameter('utm_content')) {
+        let utmParams = {
+            "utm_source": getURLParameter('utm_source'),
+            "utm_medium": getURLParameter('utm_medium'),
+            "utm_campaign": getURLParameter('utm_campaign'),
+            "utm_term": getURLParameter('utm_term'),
+            "utm_content": getURLParameter('utm_content')
+        };
+        console.log("Saving UTM Params:" + utmParams);
+        localStorage.setItem("UtmParams", JSON.stringify(utmParams));
+    }
+}
+
+function removeUtmParams() {
+    localStorage.removeItem("UtmParams");
+}
+
 //Init with identity
 if (localStorage && localStorage.getItem("id") != null && JSON.parse(<string>localStorage.getItem("id")) != null) {
     identity = JSON.parse(<string> localStorage.getItem("id"));
@@ -37,9 +59,8 @@ if (localStorage && localStorage.getItem("id") != null && JSON.parse(<string>loc
     getIdentity();
 }
 
-setInterval(repeat(), 500);
-
 function repeat() {
+    console.log("repeat() method called.");
     try {
         if (identity) {
             while (_und.profile.length > 0) {
@@ -56,6 +77,14 @@ function repeat() {
             while (_und.event.length > 0) {
                 var event = _und.event.shift();
                 event.identity = identity;
+                if(localStorage.getItem("UtmParams")) {
+                    let utmParams = JSON.parse(<string> localStorage.getItem("UtmParams"));
+                    event.attributes["utm_source"] = utmParams.utm_source;
+                    event.attributes["utm_medium"] = utmParams.utm_medium;
+                    event.attributes["utm_campaign"] = utmParams.utm_campaign;
+                    event.attributes["utm_term"] = utmParams.utm_term;
+                    event.attributes["utm_content"] = utmParams.utm_content;
+                }
                 if (event.attributes) {
                     Object.keys(event.attributes).forEach(function (key) {
                         if (event.attributes[key] instanceof Date) {
@@ -71,33 +100,21 @@ function repeat() {
                         console.log(response);
                     }
                 );
+                if(localStorage.getItem("UtmParams")) {
+                    removeUtmParams();
+                }
             }
         } else {
             getIdentity();
         }
     } catch(e) {
         console.error("Error Occurred: ", e);
+    } finally {
     }
+
 }
 
-function getURLParameter(name: any) {
-    // return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
-    if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
-        return decodeURIComponent(name[1]);
-}
+setUtmParams();
+setInterval(repeat, 1000);
 
-if (getURLParameter('utm_source') || getURLParameter('utm_medium') || getURLParameter('utm_campaign')
-    || getURLParameter('utm_term') || getURLParameter('utm_content')) {
-    _und.event.push({
-        "name": "utm",
-        "attributes": {
-            "utm_source": getURLParameter('utm_source'),
-            "utm_medium": getURLParameter('utm_medium'),
-            "utm_campaign": getURLParameter('utm_campaign'),
-            "utm_term": getURLParameter('utm_term'),
-            "utm_content": getURLParameter('utm_content')
-        }
-    });
-    console.log(getURLParameter('utm_source'), getURLParameter('utm_medium'), getURLParameter('utm_campaign')
-        , getURLParameter('utm_term'), getURLParameter('utm_content'))
-}
+
